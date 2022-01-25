6a8e81c7e409f3c68b5f87521a3f390e04956138
thecodingking10 <67853976+thecodingking10@users.noreply.github.com>
2022-01-25 16:53:59 +0530

    done

---
+++ malife.py
@@ -0,0 +1,14 @@
+def swapfiledata():
+    file1 = input("Enter file name 1")
+    file2 = input("Enter file name 2")
+    with open(file1, "r") as a:
+        data_a = a.read()
+    with open(file2, "r") as b:
+        data_b = b.read()
+    with open(file1, "w") as a:
+        a.write(data_b)
+    with open(file2, "w") as b:
+        b.write(data_a)
+
+
+swapfiledata()
